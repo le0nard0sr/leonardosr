7166b7e649fad609ca092d209028f55ca28a49b7
Leonardo Ribeiro <le0nard0sr@github.com>
2026-05-10 18:17:22 -0300

    fix: expõe dimensões de mídia no dto

--- apps/api/src/main/java/br/com/leonardosr/api/domain/MediaAsset.java
+++ apps/api/src/main/java/br/com/leonardosr/api/domain/MediaAsset.java
@@ -130,8 +130,16 @@ public class MediaAsset {
     return deletedAt;
   }
 
+  public Integer getWidth() {
+    return width;
+  }
+
   public void setWidth(Integer width) {
     this.width = width;
+  }
+
+  public Integer getHeight() {
+    return height;
   }
 
   public void setHeight(Integer height) {
