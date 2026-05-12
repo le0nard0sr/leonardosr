ae8e98c58b89d9b282c74db4ac5414f81292f8ac
Leonardo Ribeiro <le0nard0sr@github.com>
2026-05-11 23:14:26 -0300

    chore: simplifica postgres local no docker compose

--- apps/infra/postgres/init.sql
+++
@@ -1,3 +0,0 @@
--- Garante que a senha foi armazenada com scram-sha-256 (padrão PG17).
--- Necessário quando o volume é recriado a partir de um dump md5.
-ALTER USER leonardosr WITH PASSWORD 'leonardosr';
